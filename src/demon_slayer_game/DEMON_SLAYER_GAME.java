37a04ed511b64c037f1ff3273a2272f1ea83b331
christian.torresgav <christian.torresgav@5CD132CZKF.learnet.se>
2023-02-01 14:28:17 +0100

    Implementerat mina variabler och utilities som jag ska använda mig av. Dessutom en enemey name listan som innehåller alla mina motståndras namn. Dessutom la jag in alla mina metoder som kommer att användas för att bygga upp min kod i delar.

--- src/demon_slayer_game/DEMON_SLAYER_GAME.java
+++ src/demon_slayer_game/DEMON_SLAYER_GAME.java
@@ -13,11 +13,39 @@ public class DEMON_SLAYER_GAME {
     static int totalEnemyDamage;
     static int playerKills;
     static int totalPlayerKills = 0;
-    static String[] enemyNameList = { "Muzan Kibutsuji", "Akaza", "Doma", "Gyutaro", "Daki", "Rui", "Enmu",
-            "Swamp demon", "Yahaba", "Susamaru", "Nakime", "Kyogai", "Erisu" };
+    static String[] enemyNameList = { "Muzan Kibutsuji", "Akaza", "Doma", "Gyutaro", "Daki", "Rui", "Enmu","Swamp demon", "Yahaba", "Susamaru", "Nakime", "Kyogai", "Erisu" };
+    static ArrayList<String> playerName_list = new ArrayList<String>();
+    static ArrayList<Integer> kills = new ArrayList<Integer>();
+    static String difficulty = "NORMAL";
+    static double enemyDiffficultyMultiplyer = 1;
+    static double playerDifficultyMultiplier = 1.2;
+    static int enemyNameListIndex = -1;
+    static int gameRound = 1;
     
     public static void main(String[] args) {
         
     }
+    static void GAMEMENU(){
+        
+    }
+    static void DIFFICULTY(){
+        
+    }
+    static void BATTLE(){
+        
+    }
+    static void ATTACK(){
+        
+    }
+    static void WEAPONSTATS(){
+        
+    }
+    static void SCOREBOARD(){
+        
+    }
+    public static void CLEARSCREEN(){
+        System.out.print("\033[H\033[2J");
+    }
+    
     
 }
