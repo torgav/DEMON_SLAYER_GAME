5c94812159d260c9ecbad447e7f77bd04fae8481
christian.torresgav <christian.torresgav@5CD132CZKF.learnet.se>
2023-02-06 23:28:13 +0100

    Jag har ny byggt up min start menu som är central huben där användaren kan ha tillgång till de mest nödvändiga metoder, dessutom la jag in text som kommer att visas i spelet. Jag la dessa i sina egna metoder för att jag gillar inte när det är så mycket text mitt i min kod som tar upp så mycket plats så för att enklare att läsa så gjorde jag på detta sätt.

--- src/demon_slayer_game/DEMON_SLAYER_GAME.java
+++ src/demon_slayer_game/DEMON_SLAYER_GAME.java
@@ -7,17 +7,42 @@ import java.util.Scanner;
 public class DEMON_SLAYER_GAME{
     // Spel variabler
     static Scanner sc = new Scanner(System.in);
-    boolean running = true;
+    static boolean running = true;
     
     // Spelar variabler
     static ArrayList<String> playerNameList = new ArrayList<String>();
     static ArrayList<Integer> playerKillList = new ArrayList<Integer>();
-    
-    
+   
     // Fiende variablar
     
     public static void main(String[] args){
-        
+        //En simpel main som är kärnan av spelet.
+        clearScreen();
+        System.out.println("                                                                       DEMON SLAYER: BATTLE");
+        while (running) {
+            System.out.println("\tSTART MENU");
+            System.out.println("[1] BATTLE");
+            System.out.println("[2] DIFFICULTY");
+            System.out.println("[3] SCOREBOARD");
+            System.out.print("INPUT: ");
+            String input = sc.nextLine();
+            
+            //En if-sats för att kolla vad användaren la in som input.
+            if (input.equals("1")) {
+                text();
+                attack();
+                break;
+            } else if (input.equals("2")) {
+                difficulty();
+                continue;
+            } else if (input.equals("3")) {
+                scoreboard();
+            } else {
+                //Dock självklart om användaren stoppar in random text och fel number så säger programet till och låter hen försöka igen.
+                clearScreen();
+                System.out.println("                                                                    !!PLEASE ENTER A VALID INPUT!!");
+            }
+        }
     }
     
     static void attack(){
@@ -41,11 +66,40 @@ public class DEMON_SLAYER_GAME{
     }
     
     static void weaponStats(){
-        
+        //Programet informerar användaren så att de kan strategis använda sina attacker.
+        clearScreen();
+        System.out.println("LITE ATTACK:");
+        System.out.println("    - 75% Hit, 10% Miss, 10% Crit");
+        System.out.println("NORMAL ATTACK:");
+        System.out.println("    - 65% Hit, 30% Miss, 5% Crit");
+        System.out.println("HEAVY ATTACK:");
+        System.out.println("    - 27.5% Hit, 70% Miss, 2.5% Crit");
+        System.out.print("\nPRESS ENTER TO RETURN");
+        sc.nextLine();
+        clearScreen();
     }
     
     static void text(){
-        
+        //En simpel förklaring av spelet så att användaren vet vad hen gör och dens mål.
+            clearScreen();
+        System.out.println(
+                "---------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out
+                .println("                                                                                     INFO:");
+        System.out.println(
+                "                                                 WELCOME PLAYER, YOU HAVE ENTERED THE BATTLE ARENA WHERE YOU FIGHT UNTIL YOU DIE");
+        System.out.println(
+                "                                              EVERY ROUND YOU WILL FACE A NEW DEMON AND GET TO CHOSSE BETWEEN 3 ATTACKS TO KILL IT");
+        System.out.println(
+                "                                                 IF YOU SUCCESFULLY KILL THE DEMON YOU WILL BE REWARDED A AMOUNT OF HEALTH BACK");
+        System.out.println(
+                "                                                             THE LONGEST SURVIVING PLAYER IS THE ULTIMATE DEMON SLAYER ");
+        System.out.println(
+                "\n                                                                   PLEASE PRESS ENTER TO START YOUR BATTLE");
+        System.out.println(
+                "----------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        sc.nextLine();
+        clearScreen();
     }
     
     public static void clearScreen(){
