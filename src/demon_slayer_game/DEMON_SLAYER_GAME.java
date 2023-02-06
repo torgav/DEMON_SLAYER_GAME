f4a75a93b4f8569a732e6379b1c49862216ed237
christian.torresgav <christian.torresgav@5CD132CZKF.learnet.se>
2023-02-06 23:13:08 +0100

    Jag har på börjat mitt spel med min nya tanke av mallen och jag har lagt in alla metoder som ska vara med och användas, dessutom försöker jag organisera vart variablar går och sånt.

---
+++ src/demon_slayer_game/DEMON_SLAYER_GAME.java
@@ -0,0 +1,60 @@
+package demon_slayer_game;
+
+// Verktyg
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DEMON_SLAYER_GAME{
+    // Spel variabler
+    static Scanner sc = new Scanner(System.in);
+    boolean running = true;
+    
+    // Spelar variabler
+    static ArrayList<String> playerNameList = new ArrayList<String>();
+    static ArrayList<Integer> playerKillList = new ArrayList<Integer>();
+    
+    
+    // Fiende variablar
+    
+    public static void main(String[] args){
+        
+    }
+    
+    static void attack(){
+        
+    }
+    
+    static void playerAttack(){
+        
+    }
+    
+    static void enemyAttack(){
+        
+    }
+    
+    static void difficulty(){
+        
+    }
+    
+    static void scoreboard(){
+        
+    }
+    
+    static void weaponStats(){
+        
+    }
+    
+    static void text(){
+        
+    }
+    
+    public static void clearScreen(){
+        // Detta är för att hålla consolen en bit ren och inte överflödda med text.
+        System.out.println("\n\n\n\n");
+    }
+    
+    
+    
+}
+
+
